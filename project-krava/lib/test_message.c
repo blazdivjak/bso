c63a6ac48c39219a076637ee8aec9d97d494b8f3
Gregor Cimerman <gregor@cime.si>
2016-05-22 19:21:00 +0200

    Added test program for testing message struct

---
+++ project-krava/lib/test_message.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "libmessage.h"
+
+// to run this programm use: gcc -o test_message test_message.c libmessage.c && ./test_message
+int main()
+{
+   
+	Message m;
+	m = resetMessage(m);
+
+	printf("Temps: %d Accelerations: %d, Batterys: %d\n", m.tempsCount, m.accelerationsCount, m.batteriesCount);
+
+
+	printf("Adding battery reading \n");
+	m = addBattery(m, 10);
+	printf("Temps: %d Accelerations: %d, Batterys: %d\n", m.tempsCount, m.accelerationsCount, m.batteriesCount);
+
+	printf("Adding temperature reading \n");
+	m = addTemperature(m, 11);
+	printf("Temps: %d Accelerations: %d, Batterys: %d\n", m.tempsCount, m.accelerationsCount, m.batteriesCount);
+
+	printf("Adding acceleration reading \n");
+	m = addAcceleration(m, 12);
+	printf("Temps: %d Accelerations: %d, Batterys: %d\n", m.tempsCount, m.accelerationsCount, m.batteriesCount);
+
+	printf("Calculating encoded data size \n");
+	int size = getEncodeDataSize(m);
+	printf("Encoded data size is: %d\n", size);
+
+	printf("Encoding data to array of ints \n");
+	int * enc = encodeData(m);
+	int i;
+	for(i = 0; i < size; i++) {
+    	printf("%d ", enc[i]);
+    }
+    printf("\n");
+
+    printf("Printing message\n");
+    printMessage(m);
+
+	printf("Reseting all readings \n");
+	m = resetMessage(m);
+	printf("Temps: %d Accelerations: %d, Batterys: %d\n", m.tempsCount, m.accelerationsCount, m.batteriesCount);
+
+	return 0;
+}
